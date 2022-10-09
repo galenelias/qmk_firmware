fd17b0d4266ec1127e10c7eb6cff82bb318e8f9b
Chad Austin <chad@chadaustin.me>
2022-10-09 12:10:26 -0600

    transpose the matrix so matrix_row_t fits in 8 bits

--- keyboards/handwired/sculpt/config.h
+++ keyboards/handwired/sculpt/config.h
@@ -29,11 +29,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT         Wired Sculpt
 #define DESCRIPTION     Microsoft Sculpt Wired Conversion
 
-#define MATRIX_ROWS 8
-#define MATRIX_COLS 18
+#define MATRIX_ROWS 18
+#define MATRIX_COLS 8
 
-#define MATRIX_ROW_PINS {F5, F6, F7, A0, A1, A2, A3, A4}
-#define MATRIX_COL_PINS {A5, A6, A7, C7, C6, C5, C4, C3, C2, C1, C0, E1, E0, D7, D6, D5, D4, D3}
+#define MATRIX_ROW_PINS {A5, A6, A7, C7, C6, C5, C4, C3, C2, C1, C0, E1, E0, D7, D6, D5, D4, D3}
+#define MATRIX_COL_PINS {F5, F6, F7, A0, A1, A2, A3, A4}
 #define DIP_SWITCH_PINS {F4}
 #define UNUSED_PINS {/*...*/}
 
--- keyboards/handwired/sculpt/sculpt.h
+++ keyboards/handwired/sculpt/sculpt.h
@@ -12,13 +12,23 @@
     k6H, k3F, k4G, k6B, k68, k46, k43, k60, k48, k64, k61 \
 ) \
 { \
-    {KC_NO, k01,   KC_NO, k03, k04,   k05,   KC_NO, k07,   k08,   k09, k0A, k0B, k0C,   k0D,   KC_NO, KC_NO, KC_NO, KC_NO}, \
-    {KC_NO, k11,   KC_NO, k13, k14,   k15,   KC_NO, k17,   k18,   k19, k1A, k1B, k1C,   k1D,   KC_NO, k1F,   KC_NO, KC_NO}, \
-    {KC_NO, k21,   KC_NO, k23, k24,   k25,   KC_NO, k27,   KC_NO, k29, k2A, k2B, k2C,   k2D,   KC_NO, k2F,   KC_NO, KC_NO}, \
-    {KC_NO, k31,   KC_NO, k33, k34,   k35,   KC_NO, k37,   k38,   k39, k3A, k3B, KC_NO, k3D,   KC_NO, k3F,   KC_NO, KC_NO}, \
-    {KC_NO, KC_NO, KC_NO, k43, k44,   k45,   k46,   KC_NO, k48,   k49, k4A, k4B, k4C,   k4D,   KC_NO, KC_NO, k4G,   KC_NO}, \
-    {KC_NO, k51,   k52,   k53, KC_NO, k55,   KC_NO, k57,   KC_NO, k59, k5A, k5B, k5C,   k5D,   k5E,   KC_NO, KC_NO, KC_NO}, \
-    {k60,   k61,   KC_NO, k63, k64,   KC_NO, KC_NO, KC_NO, k68,   k69, k6A, k6B, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, k6H}, \
-    {KC_NO, k71,   KC_NO, k73, k74,   k75,   KC_NO, k77,   k78,   k79, k7A, k7B, k7C,   k7D,   KC_NO, k7F,   KC_NO, KC_NO} \
+    {KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, k60,   KC_NO}, \
+    {k01,   k11,   k21,   k31,   KC_NO, k51,   k61,   k71}, \
+    {KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, k52,   KC_NO, KC_NO}, \
+    {k03,   k13,   k23,   k33,   k43,   k53,   k63,   k73}, \
+    {k04,   k14,   k24,   k34,   k44,   KC_NO, k64,   k74}, \
+    {k05,   k15,   k25,   k35,   k45,   k55,   KC_NO, k75}, \
+    {KC_NO, KC_NO, KC_NO, KC_NO, k46,   KC_NO, KC_NO, KC_NO}, \
+    {k07,   k17,   k27,   k37,   KC_NO, k57,   KC_NO, k77}, \
+    {k08,   k18,   KC_NO, k38,   k48,   KC_NO, k68,   k78}, \
+    {k09,   k19,   k29,   k39,   k49,   k59,   k69,   k79}, \
+    {k0A,   k1A,   k2A,   k3A,   k4A,   k5A,   k6A,   k7A}, \
+    {k0B,   k1B,   k2B,   k3B,   k4B,   k5B,   k6B,   k7B}, \
+    {k0C,   k1C,   k2C,   KC_NO, k4C,   k5C,   KC_NO, k7C}, \
+    {k0D,   k1D,   k2D,   k3D,   k4D,   k5D,   KC_NO, k7D}, \
+    {KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, k5E,   KC_NO, KC_NO}, \
+    {KC_NO, k1F,   k2F,   k3F,   KC_NO, KC_NO, KC_NO, k7F}, \
+    {KC_NO, KC_NO, KC_NO, KC_NO, k4G,   KC_NO, KC_NO, KC_NO}, \
+    {KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, k6H,   KC_NO}, \
 }
 #endif
